e90237d9375d0e12f98c6af1d193a28e44c3e2e8
Mabel Samuel <mapleberrysamuel@gmail.com>
2024-09-12 15:47:47 +0100

    modified cartmessage

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -12,7 +12,7 @@ function Root() {
       <Header />
       <main>
       {cartMessage && (
-        <div className="fixed top-0 right-1/2 px-4 py-2 w-fit text-center border-lilac rounded shadow-lg z-[57] flex justify-center items-center gap-1 ">
+        <div className="fixed top-12 left-1/2 transform -translate-x-1/2 px-4 py-2 w-fit rounded shadow-lg z-[57] flex justify-center items-center gap-2 bg-white ">
           <GrStatusGood className="text-green-500" />
           <p>{cartMessage}</p>
         </div>
