c5cc7f8718fdfd8140c4e1700bf2864058991d2a
Mabel Samuel <mapleberrysamuel@gmail.com>
2024-11-17 10:27:08 +0100

    increased chunksize

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/amabilis-e-commerce-website/',
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase limit to 1 MB
+  },
 })
